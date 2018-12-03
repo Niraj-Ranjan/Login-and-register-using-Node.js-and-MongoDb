0bee32e2ee6772863a5e431414a7a567787e2c9a
Niraj Ranjan <32240290+Niraj-Ranjan@users.noreply.github.com>
2018-12-03 15:45:20 +0530

    Update logincontroller.js

--- app/controller/logincontroller.js
+++ app/controller/logincontroller.js
@@ -62,15 +62,23 @@ app.post('/regiterToDb',urlencodedParser, function(req,res){
  MongoClient.connect(url, function(err, db) {
    var dbo = db.db("loginregister");
        dbo.collection("register").insertOne(jsonObj, function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-         res.send()
-      db.close();
+      //if (err) throw err;
+      //console.log("1 document inserted");
+         if(err){
+           console.log("error");
+           res.send("false");
+         }
+         else{
+           console.log("1 document inserted");
+            res.send("true")
+         }
+        
+      
        });
         //res.render('completeprofile',{profileData:req.body});
        });
 
-
+db.close();
   });
 
 
